ce5b31f829a598d447726ed7efe45d95f20e1273
RbalajiU0 <balaji.udayasankar@relevantz.com>
2025-03-14 18:04:58 +0530

    first commit

---
+++ calc.cs
@@ -0,0 +1,13 @@
+class task{
+    void balaji() {}
+    void aashik() {}
+    void sandhiya() {}
+    void bangaru() {}
+    public static void main(String[] args) {
+        Task task = new Task();
+        task.balaji();//balaji work
+        task.bangaru();//banagru work
+        task.aashik();//aashik work
+        task.sandhiya();//sandhiya work
+    }
+}
